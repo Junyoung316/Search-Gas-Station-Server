dd55fd279bb205428d774251ca91cb151c755fff
Jun <76056849+Junyoung316@users.noreply.github.com>
2025-11-11 16:42:32 +0900

    init
    build.gradle 설정 및 appilcaton.yml 설정

---
+++ src/test/java/com/spring/searchGasStation/SearchGasStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.searchGasStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchGasStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
